cde16c1914291f7adf531e312b9e8d64b60e3181
Sta2yan <s2_stas@mail.ru>
2022-02-27 14:45:30 +0300

    Добавьте файлы проекта.

---
+++ NameOutput/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            char userSympbol;
+            int lenghtUserSymbol;
+            int convertLengthNumber = 2;
+
+            Console.Write("Введите имя: ");
+            userName = Console.ReadLine();
+
+            lenghtUserSymbol = userName.Length + convertLengthNumber;
+
+            Console.Write("Введите символ: ");
+            userSympbol = Console.ReadKey().KeyChar;
+            Console.Clear();
+
+            for (int i = 0; i < lenghtUserSymbol; i++)
+                Console.Write(userSympbol);
+
+            Console.WriteLine("\n" + userSympbol + userName + userSympbol);
+
+            for (int i = 0; i < lenghtUserSymbol; i++)
+                Console.Write(userSympbol);
+
+
+            Console.ReadKey();
+        }
+    }
+}
